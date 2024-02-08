61ea1846080b5d1c417c364d481131b55bea07c9
Abdelmounaim Bouakouk <abdelmounaim@bouakouk.com>
2024-02-08 17:02:58 +0100

    eslint updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
     }
   ],
   rules: {
-    'svelte/no-unused-svelte-ignore': 'off'
+    'svelte/no-unused-svelte-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
   }
 }
