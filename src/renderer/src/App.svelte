0d61febc0f7f15b449df8ce750f9592e807fb258
Abdelmounaim Bouakouk <abdelmounaim@bouakouk.com>
2024-02-08 16:16:33 +0100

    stop client from tab button + cleanup

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,88 +1,108 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { Globe, Settings } from 'lucide-svelte';
-	import BrowserComponent from './components/BrowserComponent.svelte';
-	import SettingsComponent from './components/SettingsComponent.svelte';
-	import type { Character } from './characterutils';
-	import NeuzClientComponent from './components/NeuzClientComponent.svelte';
+  import { onMount } from 'svelte'
+  import { Globe, Settings, X } from 'lucide-svelte'
+  import BrowserComponent from './components/BrowserComponent.svelte'
+  import SettingsComponent from './components/SettingsComponent.svelte'
+  import type { Character } from './characterutils'
+  import NeuzClientComponent from './components/NeuzClientComponent.svelte'
 
-	let activeClientTab: string = '';
-	let lastActiveClientTab: string = '';
-	let activeOverlay: string = '';
-	let chars: Character[] = [];
-	onMount(() => {
-		loadCharacters();
-		activeClientTab = chars[0].id;
-		window.electron.ipcRenderer.on('doTabbing', function () {
-			if (lastActiveClientTab == '') return;
-			const switchToTab = lastActiveClientTab;
-			lastActiveClientTab = activeClientTab;
-			activeClientTab = switchToTab;
+  let activeClientTab: string = ''
+  let lastActiveClientTab: string = ''
+  let activeOverlay: string = ''
+  let chars: Character[] = []
+  let clients: Record<string, NeuzClientComponent> = {}
+
+  onMount(() => {
+    loadCharacters()
+    activeClientTab = chars[0].id || ''
+    window.electron.ipcRenderer.on('doTabbing', function () {
+      if (lastActiveClientTab == '') return
+      const switchToTab = lastActiveClientTab
+      lastActiveClientTab = activeClientTab
+      activeClientTab = switchToTab
       activeOverlay = ''
-		});
-	});
+    })
+  })
 
-	function loadCharacters() {
-		chars = JSON.parse(localStorage.getItem('characterStore') || '[]');
-	}
+  function loadCharacters() {
+    chars = JSON.parse(localStorage.getItem('characterStore') || '[]')
+  }
 </script>
 
 <div class="w-full h-full flex flex-col overflow-hidden">
-	<div class="flex bg-neutral-800 text-white font-semibold items-center">
-		<div class="items-center flex">
-			<button
-				on:click={() => {
-					activeOverlay = activeOverlay == 'settings' ? '' : 'settings';
-				}}
-				class="p-4 hover:bg-black/20 duration-200 border-r border-r-white/10"
-				class:bg-neutral-950={activeOverlay == 'settings'}><Settings /></button
-			>
-		</div>
+  <div class="flex bg-neutral-800 text-white font-semibold items-center">
+    <div class="items-center flex">
+      <button
+        on:click={() => {
+          activeOverlay = activeOverlay == 'settings' ? '' : 'settings'
+        }}
+        class="p-3.5 hover:bg-black/20 duration-200 border-r border-r-white/10"
+        class:bg-neutral-950={activeOverlay == 'settings'}><Settings /></button
+      >
+    </div>
 
-		<div class="items-center flex">
-			<button
-				class="p-4 hover:bg-black/20 duration-200 border-r border-r-white/10"
-				class:bg-neutral-950={activeOverlay == 'browser'}
-				on:click={() => {
-					activeOverlay = activeOverlay == 'browser' ? '' : 'browser';
-				}}><Globe /></button
-			>
-		</div>
+    <div class="items-center flex">
+      <button
+        class="p-3.5 hover:bg-black/20 duration-200 border-r border-r-white/10"
+        class:bg-neutral-950={activeOverlay == 'browser'}
+        on:click={() => {
+          activeOverlay = activeOverlay == 'browser' ? '' : 'browser'
+        }}><Globe /></button
+      >
+    </div>
 
-		<div class="flex items-center flex-1 p-2 gap-3">
-			{#each chars as char}
-				<button
-					on:click={() => {
-						lastActiveClientTab = activeClientTab;
-						activeClientTab = char.id;
-						activeOverlay = '';
-					}}
-					class="p-1 px-3 {activeClientTab == char.id
-						? 'bg-neutral-950'
-						: 'bg-white/20'} rounded flex gap-2 items-center"
-				>
-					<img class="w-4" src="jobs/{char.job}.png?asset" alt={char.job} />{char.label}</button
-				>
-			{/each}
-		</div>
-	</div>
-	<div class="flex flex-col bg-neutral-700 flex-1 relative">
-		{#each chars as char}
-			<div class="absolute left-0 w-full h-full" class:hidden={activeClientTab != char.id}>
-				<NeuzClientComponent {char}></NeuzClientComponent>
-			</div>
-		{/each}
-		<div
-			class="abosulte left-0 top-0 z-50 bg-black/80 w-full h-full flex flex-col p-4"
-			class:hidden={activeOverlay != 'browser'}
-		>
-			<BrowserComponent />
-		</div>
-		<div
-			class="abosulte left-0 top-0 z-50 bg-black/80 w-full h-full flex flex-col p-4"
-			class:hidden={activeOverlay != 'settings'}
-		>
-			<SettingsComponent on:saved={loadCharacters} />
-		</div>
-	</div>
+    <div class="flex items-center flex-1 p-2 gap-3">
+      {#each chars as char}
+        <button
+          on:click={() => {
+            lastActiveClientTab = activeClientTab
+            activeClientTab = char.id
+            activeOverlay = ''
+          }}
+          class="p-1 px-3 {activeClientTab == char.id
+            ? 'bg-neutral-950'
+            : 'bg-white/20'} rounded flex gap-2 items-center"
+        >
+          <img class="w-4" src="jobs/{char.job}.png?asset" alt={char.job} />
+          <strong class="px-2">{char.label}</strong>
+          {#if clients[char.id]?.isStarted()}
+            <button
+              on:click={(e) => {
+                clients[char.id]?.stopClient()
+                e.stopPropagation()
+              }}
+              class="p-0.5 bg-white/10 hover:bg-white/20 duration-200 rounded-md"
+            >
+              <X class="w-4 h-4 roudned" />
+            </button>
+          {:else}
+            <span class="w-5"></span>
+          {/if}
+        </button>
+      {/each}
+    </div>
+  </div>
+  <div class="flex flex-col bg-neutral-700 flex-1 relative">
+    {#each chars as char}
+      <div class="absolute left-0 w-full h-full" class:hidden={activeClientTab != char.id}>
+        <NeuzClientComponent
+          on:updated={() => (clients = clients)}
+          bind:this={clients[char.id]}
+          {char}
+        ></NeuzClientComponent>
+      </div>
+    {/each}
+    <div
+      class="abosulte left-0 top-0 z-50 bg-black/80 w-full h-full flex flex-col p-4"
+      class:hidden={activeOverlay != 'browser'}
+    >
+      <BrowserComponent />
+    </div>
+    <div
+      class="abosulte left-0 top-0 z-50 bg-black/80 w-full h-full flex flex-col p-4"
+      class:hidden={activeOverlay != 'settings'}
+    >
+      <SettingsComponent on:saved={loadCharacters} />
+    </div>
+  </div>
 </div>
--- src/renderer/src/components/NeuzClientComponent.svelte
+++ src/renderer/src/components/NeuzClientComponent.svelte
@@ -1,30 +1,40 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import type { Character } from '../characterutils';
-	import { X } from 'lucide-svelte';
+  import { createEventDispatcher, onMount } from 'svelte'
+  import type { Character } from '../characterutils'
 
-	export let char: Character;
-	let partition: string = '';
-    let started : boolean;
-	onMount(() => {
-		partition = `persist:${char.id}`;
-	});
+  const dispatch = createEventDispatcher()
+  export let char: Character
+  let partition: string = ''
+  let started: boolean = false
+  onMount(() => {
+    partition = `persist:${char.id}`
+  })
+
+  export const starClient = () => {
+    started = true
+    dispatch('updated')
+  }
+
+  export const stopClient = () => {
+    started = false
+    dispatch('updated')
+  }
+
+  export const isStarted = () => {
+    return started
+  }
 </script>
 
 {#if partition != ''}
-{#if started}
-<button on:click={() => started = false} class="bg-black/60 hover:bg-black/80 rounded m-1 p-1 text-white absolute left-0 top-0 flex items-center justify-center">
-    <X class="w-4 h-4"/>
-</button>
-	<webview
-		class="w-full h-full"
-		src="https://universe.flyff.com/play"
-		{partition}
-	></webview>
-    {:else}
+  {#if started}
+    <webview class="w-full h-full" src="https://universe.flyff.com/play" {partition}></webview>
+  {:else}
     <div class="w-full h-full flex items-center flex-col gap-2 justify-center">
-        <img src="logofull.png">
-        <button class="p-4 w-1/3 bg-black/50 hover:bg-black/80 duration-200 text-white" on:click={() => started = true}>Play ({char.label})</button>
+      <img src="logofull.png" alt="Flyff Universe Logo" />
+      <button
+        class="p-4 w-1/3 bg-black/50 hover:bg-black/80 duration-200 text-white"
+        on:click={starClient}>Play ({char.label})</button
+      >
     </div>
-    {/if}
+  {/if}
 {/if}
